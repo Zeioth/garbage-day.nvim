74ff8692d2ab3512bc1cc9a20ff2127c742b9a70
Zeioth <zeioth@hotmail.com>
2023-11-27 06:33:04 +0100

    ♻️chore: Overhead slightly reduced.

--- lua/garbage-day/config.lua
+++ lua/garbage-day/config.lua
@@ -6,7 +6,7 @@ function M.set(opts)
   M.excluded_lsp_clients = opts.excluded_lsp_clients or { "null-ls", "jdtls" }
   M.grace_period = opts.grace_period or (60 * 15) -- seconds
   M.notifications = opts.notifications or false
-  M.retries = opts.retries or 5 -- times
+  M.retries = opts.retries or 3 -- times
   M.timeout = opts.timeout or 1000 -- ms
   M.wakeup_delay = opts.wakeup_delay or 0 -- ms
 end
